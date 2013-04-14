76ac7123e9829368a2b6f41301991006d2085150
Thomas Thomassen <thomas@thomthom.net>
2013-04-14 20:45:10 +0200

    Changed: Moved TT_Lib checks to core.rb

--- TT_ArchitectTools/core.rb
+++ TT_ArchitectTools/core.rb
@@ -5,6 +5,31 @@
 #
 #-------------------------------------------------------------------------------
 
+require 'sketchup.rb'
+begin
+  require 'TT_Lib2/core.rb'
+rescue LoadError => e
+  module TT
+    if @lib2_update.nil?
+      url = 'http://www.thomthom.net/software/sketchup/tt_lib2/errors/not-installed'
+      options = {
+        :dialog_title => 'TT_Lib² Not Installed',
+        :scrollable => false, :resizable => false, :left => 200, :top => 200
+      }
+      w = UI::WebDialog.new( options )
+      w.set_size( 500, 300 )
+      w.set_url( "#{url}?plugin=#{File.basename( __FILE__ )}" )
+      w.show
+      @lib2_update = w
+    end
+  end
+end
+
+
+#-------------------------------------------------------------------------------
+
+if defined?( TT::Lib ) && TT::Lib.compatible?( '2.7.0', 'Architect Tools' )
+
 module TT::Plugins::ArchitectTools
   
   
@@ -2652,14 +2677,36 @@ module TT::Plugins::ArchitectTools
   
   ### DEBUG ### ----------------------------------------------------------------
   
-  # TT::Plugins::ArchitectTools.reload
-  def self.reload
+  # @note Debug method to reload the plugin.
+  #
+  # @example
+  #   TT::Plugins::ArchitectTools.reload
+  #
+  # @param [Boolean] tt_lib
+  #
+  # @return [Integer]
+  # @since 1.0.0
+  def self.reload( tt_lib = false )
+    original_verbose = $VERBOSE
+    $VERBOSE = nil
+    TT::Lib.reload if tt_lib
+    # Core file (this)
     load __FILE__
+    # Supporting files
+    #x = Dir.glob( File.join(PATH, '*.{rb,rbs}') ).each { |file|
+    #  load file
+    #}
+    #x.length
+  ensure
+    $VERBOSE = original_verbose
   end
   
 end # module
 
+end # if TT_Lib
 
 #-------------------------------------------------------------------------------
+
 file_loaded( __FILE__ )
+
 #-------------------------------------------------------------------------------
--- tt_architect_tools.rb
+++ tt_architect_tools.rb
@@ -6,25 +6,7 @@
 #-------------------------------------------------------------------------------
 
 require 'sketchup.rb'
-begin
-  require 'TT_Lib2/core.rb'
-rescue LoadError => e
-  module TT
-    if @lib2_update.nil?
-      url = 'http://www.thomthom.net/software/sketchup/tt_lib2/errors/not-installed'
-      options = {
-        :dialog_title => 'TT_Lib² Not Installed',
-        :scrollable => false, :resizable => false, :left => 200, :top => 200
-      }
-      w = UI::WebDialog.new( options )
-      w.set_size( 500, 300 )
-      w.set_url( "#{url}?plugin=#{File.basename( __FILE__ )}" )
-      w.show
-      @lib2_update = w
-    end
-  end
-end
-
+require 'extensions.rb'
 
 #-------------------------------------------------------------------------------
 
@@ -37,40 +19,23 @@ module TT
   # Plugin information
   PLUGIN_ID       = 'TT_ArchitectTools'.freeze
   PLUGIN_NAME     = 'Architect Tools'.freeze
-  PLUGIN_VERSION  = '2.0.0'.freeze
-  
-  # Version information
-  RELEASE_DATE    = '03 Jan 13'.freeze
+  PLUGIN_VERSION  = '2.1.0'.freeze
   
   # Resource paths
   PATH_ROOT   = File.dirname( __FILE__ ).freeze
-  PATH        = File.join( PATH_ROOT, 'TT_ArchitectTools' ).freeze
+  PATH        = File.join( PATH_ROOT, 'TT_Architect_Tools' ).freeze
   PATH_ICONS  = File.join( PATH, 'Icons' ).freeze
-
-  
-  ### LIB FREDO UPDATER ### ----------------------------------------------------
-  
-  def self.register_plugin_for_LibFredo6
-    {
-      :name => PLUGIN_NAME,
-      :author => 'thomthom',
-      :version => PLUGIN_VERSION.to_s,
-      :date => RELEASE_DATE,   
-      :description => 'Tools for generating buildings, roads, terrain and etc from siteplans.',
-      :link_info => 'http://sketchucation.com/forums/viewtopic.php?t=30512'
-    }
-  end
   
   
   ### EXTENSION ### ------------------------------------------------------------
   
-  if defined?( TT::Lib ) && TT::Lib.compatible?('2.7.0', 'Architect Tools')
+  unless file_loaded?( __FILE__ )
     loader = File.join( PATH, 'core.rb' )
     ex = SketchupExtension.new( PLUGIN_NAME, loader )
-    ex.description = self.register_plugin_for_LibFredo6[:description]
-    ex.version = PLUGIN_VERSION
-    ex.copyright = 'Thomas Thomassen © 2010-2013'
-    ex.creator = 'Thomas Thomassen (thomas@thomthom.net)'
+    ex.description = 'Tools for generating buildings, roads, terrain and etc from siteplans.'
+    ex.version     = PLUGIN_VERSION
+    ex.copyright   = 'Thomas Thomassen © 2010-2013'
+    ex.creator     = 'Thomas Thomassen (thomas@thomthom.net)'
     Sketchup.register_extension( ex, true )
   end
   
